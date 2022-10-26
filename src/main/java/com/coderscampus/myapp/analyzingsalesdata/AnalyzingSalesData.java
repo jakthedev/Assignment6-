5bf0c6943d95887a5fb842223ca6723f64664e46
JD <jdakemp@gmail.com>
2022-10-26 12:03:40 -0400

    still trying to get the sales amount on its own in a seperate Integer arrayList

--- src/main/java/com/coderscampus/myapp/analyzingsalesdata/AnalyzingSalesData.java
+++ src/main/java/com/coderscampus/myapp/analyzingsalesdata/AnalyzingSalesData.java
@@ -5,7 +5,11 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import java.time.YearMonth;
 import java.time.format.DateTimeFormatter;
 import com.coderscampus.myapp.service.SalesService;
@@ -61,14 +65,40 @@ public class AnalyzingSalesData {
 				// System.out.println(sale.toStringM3());
 				model3[i++] = sale;
 				// System.out.println(i);
+				
+				List<Sales> model3S =  Arrays.stream(model3).collect(Collectors.toList()); 
+				
+				List<Integer> model3SA = model3S.stream()
+						.map(amount -> amount.getNumMonthlySales())
+						.collect(Collectors.toList());
+				
+				for (Integer saleA : model3SA) {
+					System.out.println(saleA); 
+					System.out.println("hey");
+				}
 
-				System.out.println(sale2.toStringMS());
+				//System.out.println(sale2.toStringMS());
 				modelS[j++] = sale2;
 				// System.out.println(j);
 
 				 //System.out.println(sale3.toStringMX());
 				modelX[d++] = sale3;
 				// System.out.println(d);
+				
+				
+				//List<Integer> model3S = new ArrayList<Integer>(); 
+//				List<Sales> model3S =  Arrays.stream(model3).collect(Collectors.toList()); 
+//				
+//				List<Integer> model3SA = model3S.stream()
+//						.map(amount -> amount.getNumMonthlySales())
+//						.collect(Collectors.toList());
+//				
+//				for (Integer saleA : model3SA) {
+//					System.out.println(saleA); 
+//					System.out.println("hey");
+//				}
+				
+				
 			}
 		} catch (NullPointerException e) {
 			return;
@@ -78,8 +108,27 @@ public class AnalyzingSalesData {
 				salesReader1.close();
 				salesReader2.close();
 		}
-
 		
+
+//	private static void model3sales(Sales[] model3) {
+//		List<Sales[]> model3L = new ArrayList<>();
+//		
+//		List<Integer> list = new ArrayList<Integer>();
+//		
+//		List<Integer> model3S = model3L.stream() 
+//																.map(amount -> amount.getNumMonthlySales())
+//																.collect(Collectors.toList());
+//		
+//		model3S.stream().forEach(amount -> System.out.println(amount));
+		
+		//		List<Sales> model3s = new ArrayList<>();
+//		List<Integer> model3L = model3s.stream(); 
+		
+//		List<Integer> model3Sales1 = model3L.stream()
+//					   .map(sale -> sale.getNumMonthlySales()).collect(Collectors.toList());
+		
+//		model3Sales1.stream().forEach(amount -> System.out.println(amount)); 
+//		System.out.println("trap");
 	}
 
 }
